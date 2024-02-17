6893fd711d2f6640337c3e5033cbb98778cbd6cb
Nitesh Sunku <nsunku99@gmail.com>
2024-02-17 12:58:11 -0800

    min width for pictures on all users page

--- src/app/_components/User.tsx
+++ src/app/_components/User.tsx
@@ -25,21 +25,21 @@ export default function User({
   company,
 }: UserInfo) {
   return (
-    <div className='flex flex-col items-center gap-5 p-10 border-4 border-purple-800 rounded-xl md:flex-row'>
+    <div className='flex flex-col items-center justify-center gap-5 px-5 py-10 bg-purple-100 shadow-xl rounded-xl md:flex-row md:flex-wrap'>
       <div>
         <Image
-          className='m-3'
+          className='m-3 min-w-[125px] h-full'
           src={image}
           alt={`Picture of ${firstName}`}
           height={150}
           width={150}
         />
       </div>
-      <div className='flex flex-col items-start justify-around gap-5'>
+      <div className='flex flex-col items-center justify-around gap-5 md:items-start'>
         <p className='text-3xl font-bold '>{`${firstName} ${lastName}`} </p>
         <Link
           href={`/users/${id}`}
-          className='p-3 text-xl font-semibold bg-purple-400 border-2 border-purple-600 rounded-xl hover:opacity-75'
+          className='p-3 bg-purple-400 border-2 rounded-xl hover:opacity-75'
         >
           Meet {firstName}!
         </Link>
