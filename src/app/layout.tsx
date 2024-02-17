de2309af72c1f008b7ca4a26b9b8a29305e087d3
Nitesh Sunku <nsunku99@gmail.com>
2024-02-16 19:20:32 -0800

    default page styling on main body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <main>{children}</main>
+        <main className='p-20'>{children}</main>
       </body>
     </html>
   );
