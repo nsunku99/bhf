8e5b3c6d269a32f185438dcc62e5fefa257a0224
Nitesh Sunku <nsunku99@gmail.com>
2024-02-17 12:59:08 -0800

    added generate static params function to make dynamic routes statically generated at build time

--- src/app/users/[user]/page.tsx
+++ src/app/users/[user]/page.tsx
@@ -3,6 +3,15 @@ import Post from '@/app/_components/Post';
 import type { PostInfo } from '@/app/_components/Post';
 import { Fragment } from 'react';
 
+export async function generateStaticParams() {
+  const res = await fetch('https://dummyjson.com/users?limit=20');
+  const { users } = await res.json();
+
+  return users.map(({ id }: { id: number }) => ({
+    slug: id,
+  }));
+}
+
 export default async function UserPage({
   params,
 }: {
@@ -10,34 +19,44 @@ export default async function UserPage({
 }) {
   const { user } = params;
 
-  const response = await fetch(`https://dummyjson.com/users/${user}`);
-  const data = await response.json();
-  console.log(data);
+  const res = await fetch(`https://dummyjson.com/users/${user}`);
+  const data = await res.json();
+  // console.log(data);
 
-  const postResponse = await fetch(`https://dummyjson.com/users/${user}/posts`);
-  const { posts: postData } = await postResponse.json();
+  const postsRes = await fetch(`https://dummyjson.com/users/${user}/posts`);
+  const { posts: postData } = await postsRes.json();
+  console.log(postData);
 
-  // console.log(posts, posts.posts[0]);
-  const posts = postData.map(({ title, body, tags }: PostInfo) => {
-    return (
-      <Fragment key={title}>
-        <Post title={title} body={body} tags={tags} />
-      </Fragment>
+  const posts =
+    postData && postData.length ? (
+      postData.map(({ title, body, tags }: PostInfo) => {
+        return (
+          <Fragment key={title}>
+            <Post title={title} body={body} tags={tags} />
+          </Fragment>
+        );
+      })
+    ) : (
+      <p className='p-32 text-center text-slate-600'>
+        {data.firstName} has not made any posts yet
+      </p>
     );
-  });
 
   return (
     <div>
-      <div>
+      <div className='flex'>
         <Image
+          className='m-5'
           src={data.image}
           alt={`Image of ${data.firstName}`}
           height={150}
           width={150}
         />
-        <p>
-          {data.firstName} {data.lastName}
-        </p>
+        <div className='m-5'>
+          <p className='text-3xl'>
+            {data.firstName} {data.lastName}
+          </p>
+        </div>
       </div>
       {posts}
     </div>
