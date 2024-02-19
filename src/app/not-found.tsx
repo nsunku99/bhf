f0f643c2aa65db8dad66085ba64dd193bd5cb39d
Nitesh Sunku <nsunku99@gmail.com>
2024-02-18 18:03:18 -0800

    one not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,4 @@
-// Custom Not Found for Non Existent User
+// Not Found
 
 import Link from 'next/link';
 import Image from 'next/image';
@@ -8,7 +8,7 @@ export default function NotFound() {
   return (
     <div className='flex flex-col items-center justify-center'>
       <Image className='w-auto h-1/3' src={cat404} alt='Not Found' />
-      <p>User Does Not Exist</p>
+      <p>Sorry, Page Does Not Exist</p>
       <Link className='p-3 m-3 text-white bg-purple-500 rounded-xl' href='/'>
         Go Back to User List
       </Link>
